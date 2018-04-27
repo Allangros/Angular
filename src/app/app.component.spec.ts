3a7ca51199621d84ef4d886df83531ab598dfd4f
allan <allan.gros@itech.fr>
2018-04-27 17:00:48 +0200

    tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -8,7 +8,7 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+  /* it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
@@ -23,5 +23,16 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+  })); */
+  it('title = mon app ?'), () => {
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.componentInstance
+    expect(app.title).toContain('app')
+  }
+  it('que contient h1 ?'), () => {
+    const fixture = TestBed.createComponent(AppComponent)
+    fixture.detectChanges()
+    const tpl = fixture.nativeElement
+    expect(tpl.querySelector('h1').textContent).toContain('app')
+  }
 });
